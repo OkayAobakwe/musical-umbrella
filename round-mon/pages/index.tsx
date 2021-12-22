515d3f3d0b7f6e6538843801b5c0420734fb2f7f
aobakwe sometimes <okayaobakwe@gmail.com>
2021-12-22 18:54:47 +0200

    chore(ui): organise the ui structure

--- round-mon/pages/index.tsx
+++ round-mon/pages/index.tsx
@@ -2,7 +2,15 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <div className='text-2xl text-red-200'>hello world</div>
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className="text-2xl text-center">Which Pokemon is Hotter?</div>
+      <div className="p-3"/>
+      <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
+        <div className="w-16 h-16 bg-red-200"/>
+        <div className="p-8">vs</div>
+        <div className="w-16 h-16 bg-red-200"/>
+      </div>
+    </div>
   )
 }
 
