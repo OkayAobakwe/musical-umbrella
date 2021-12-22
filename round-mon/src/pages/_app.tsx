ac2b8e469ea6678a89847d8a2b442fb503a03226
aobakwe sometimes <okayaobakwe@gmail.com>
2021-12-22 19:43:22 +0200

    feat(trpc): setup trpc

--- round-mon/src/pages/_app.tsx
+++ round-mon/src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import 'tailwindcss/tailwind.css'
 import '../styles/global.css'
-import type { AppProps } from 'next/app'
+import type AppProps from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: any) {
   return <Component {...pageProps}/>
 }
 
