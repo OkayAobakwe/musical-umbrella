76b22cb5777cbdd9a724819199aa188f2150c795
aobakwe sometimes <okayaobakwe@gmail.com>
2021-12-23 08:50:15 +0200

    chore(pokemon-functions): get pokemon ids

---
+++ round-mon/src/utils/getRandomPokemon.ts
@@ -0,0 +1,15 @@
+
+const MAX_DEX_ID = 493
+export const getRandomPokemon: (notThisOne?: number) => number = (notThisOne) => {
+  const pokedexNum = Math.floor(Math.random() * MAX_DEX_ID) + 1
+  if(pokedexNum !== notThisOne) return pokedexNum
+
+  return getRandomPokemon(notThisOne)
+}
+
+export const getOptionsToVote = () => {
+  const firstId = getRandomPokemon();
+  const secondId = getRandomPokemon(firstId)
+  
+  return [firstId, secondId]
+}
