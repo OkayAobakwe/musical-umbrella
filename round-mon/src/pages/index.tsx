f672b357b2f5b078bb6a7eea0eb813b136dfbdf6
aobakwe sometimes <okayaobakwe@gmail.com>
2021-12-25 15:54:29 +0200

    chore(loading): fix loading data

--- round-mon/src/pages/index.tsx
+++ round-mon/src/pages/index.tsx
@@ -1,7 +1,11 @@
-import { useState } from 'react'
+import { useState, FC } from 'react'
 import { getOptionsToVote } from '@/utils/getRandomPokemon'
 import { trpc } from '@/utils/trpc'
 import type { NextPage } from 'next'
+import { inferQueryResponse } from './api/trpc/[trpc]'
+
+const btn =
+  "text-gray-900 bg-white border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-gray-600 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-800"
 
 const Home: NextPage = () => {
   const [ids, setIds] = useState(() => getOptionsToVote())
@@ -13,36 +17,49 @@ const Home: NextPage = () => {
     id: secondId
   }])
 
-  if(firstPokemon.isLoading || secondPokemon.isLoading) return null;
-
   const voteForHottest = ( selected: number) => {
     //todo fire mutation to persist changes
 
     setIds(getOptionsToVote())
   }
-  const btn =
-  "text-gray-900 bg-white border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-gray-600 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-800"
 
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which Pokemon is Hotter?</div>
       <div className="p-3"/>
       <div className="p-8 flex justify-between items-center max-w-2xl">
-        <div className="w-44 h-44 flex flex-col items-center">
-          <img className="w-full"src={firstPokemon.data?.sprites.front_default || undefined} />
-          <div className="text-xl text-center capitalize">{firstPokemon.data?.name}</div>
-          <button className={btn} onClick={() => voteForHottest(firstId)}>hotter</button>
-        </div>
-        <div className="p-8">vs</div>
-        <div className="w-44 h-44 flex flex-col items-center">
-          <img src={secondPokemon.data?.sprites.front_default || undefined} className="w-full"/>
-          <div className="text-xl text-center capitalize">{secondPokemon.data?.name}</div>
-          <button className={btn} onClick={() => voteForHottest(secondId)}>hotter</button>
-        </div>
+        {!firstPokemon.isLoading && firstPokemon.data
+          && !secondPokemon.isLoading && secondPokemon.data
+          && (
+            <>
+              <PokemonListing
+                pokemon={firstPokemon.data}
+                vote={() => voteForHottest(firstId)}
+              />
+              <div className="p-8">vs</div>
+              <PokemonListing 
+                pokemon={secondPokemon.data}
+                vote={() => voteForHottest(secondId)}
+                />
+            </>
+          )
+        }
         <div className="p-4"/>
       </div>
     </div>
   )
 }
 
+type PokemonFromServer = inferQueryResponse<"get-pokemon-by-id">
+
+const PokemonListing: FC<{pokemon: PokemonFromServer, vote: () => void}> = (props) => {
+
+  return(
+    <div className="w-44 h-44 flex flex-col items-center">
+      <img className="w-full"src={props.pokemon.sprites.front_default || undefined} />
+      <div className="text-xl text-center capitalize">{props?.pokemon.name}</div>
+      <button className={btn} onClick={() => props.vote()}>hotter</button>
+    </div>
+  )
+}
 export default Home
