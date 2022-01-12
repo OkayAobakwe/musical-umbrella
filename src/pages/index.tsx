54ae406405ee264862b317a6596a594a53aa6efe
aobakwe sometimes <okayaobakwe@gmail.com>
2022-01-12 16:18:18 +0200

    chore(results-route): add the results route to the footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,9 @@ import { getOptionsToVote } from '@/utils/getRandomPokemon'
 import { trpc } from '@/utils/trpc'
 import type { NextPage } from 'next'
 import { inferQueryResponse } from './api/trpc/[trpc]'
+
 import Image from 'next/image'
+import Link from 'next/link'
 
 const btn =
   "text-gray-900 bg-white border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-gray-600 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-800"
@@ -52,7 +54,8 @@ const Home: NextPage = () => {
         <div className="p-4"/>
       </div>
       <div className='absolute bottom-0 w-full text-xl text-center'>
-        <a href="https://github.com/OkayAobakwe/musical-umbrella">Github</a>
+        <a href="https://github.com/OkayAobakwe/musical-umbrella">Github</a> {" | "}
+        <Link href="/results"><a>Results</a></Link>
       </div>
     </div>
   )
